7eb44022a2d942cac87e2abd5c32cafc4262a6ad
Adriano Souza <1994adrianosantos@gmail.com>
2023-12-16 21:17:49 -0300

    1922-2021.py

--- Python - Exercicios/1922-2021.py
+++ Python - Exercicios/1922-2021.py
@@ -1,10 +1,16 @@
 ano_atual = 2023
 nome = str(input("Digite seu nome: "))
-ano_nascimento = int(input("Digite seu ano de nascimento: "))
 
-while (ano_nascimento < 1922) or (ano_nascimento > ano_atual):
-    ano_nascimento = int(input("Digite seu ano de nascimento: "))
-
-idade = ano_atual - ano_nascimento
-print(f"A idade atual do {nome} é {idade} anos.")
+executar = True
+while(executar == True):
+    try:
+        ano_nascimento = int(input("Digite seu ano de nascimento: "))
+        if (ano_nascimento < 1922) or (ano_nascimento > 2022):
+            print("O ano precisa ser entre 1922 e 2022")
+        else:
+            idade = ano_atual - ano_nascimento
+            print(f"A idade atual do {nome} é {idade} anos.")
+            executar = False
+    except:
+        print("Os anos precisam ser escritos apenas com números.")
 
